0df89fef956485cf48424e669e26e28691c563ee
ashutosh4023 <ashutosh.ray10@yahoo.co.in>
2025-09-13 16:03:35 -0700

    Update db.js

--- db.js
+++ db.js
@@ -9,12 +9,30 @@ function getUserById(id) {
 }
 
 function createUser(id, email, passKey) {
-    USERS.push({ id, email, passKey })
+    const existingUser = getUserByEmail(email);
+      if (existingUser) {
+    // Append new credential
+    existingUser.passKeys = existingUser.passKeys || [];
+    existingUser.passKeys.push(passKey);
+  } else {
+    // Create new user with passKeys array
+    USERS.push({
+      id,
+      email,
+      passKeys: [passKey],
+    });
+  }
+
 }
 
-function updateUserCounter(id, counter) {
-    const user = USERS.find(user => user.id === id)
-    user.passKey.counter = counter
+function updateUserCounter(userId, newCounter, credentialId) {
+  const user = getUserById(userId);
+  if (!user || !user.passKeys) return;
+
+  const credential = user.passKeys.find(pk => pk.id === credentialId);
+  if (credential) {
+    credential.counter = newCounter;
+  }
 }
 
 module.exports = {
